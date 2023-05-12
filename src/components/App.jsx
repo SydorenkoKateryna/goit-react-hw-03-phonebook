87b1ff6dd9566c187ab8945147e2c128b4198814
SydorenkoKateryna <sydorenko.k.ih@gmail.com>
2023-05-12 14:28:21 +0300

    add contacts to localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,25 @@ import ContactList from './ContactList';
 
 class App extends Component {
   state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const savedData = localStorage.getItem('contactList');
+
+    if (savedData) {
+      this.setState({ contacts: JSON.parse(savedData) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+
+    if (prevState.contacts !== contacts) {
+      localStorage.setItem('contactList', JSON.stringify(contacts));
+    }
+  }
 
   createContact = data => {
     const newContact = { id: nanoid(), ...data };
